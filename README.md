f619586972f31d156178e6c6f09b05195abf8726
Hongda Hu <hdhu@thoughtworks.com>
2014-07-06 23:11:21 +0800

    [jerry] init chef swap

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 chef-swap
 =========
 
-configure swap for you centos
+## configure swap for you centos
+
+## Usage
+
+<pre>
+{
+    "name": "swap",
+    "description": "swap configure",
+    "default_attributes": {
+    },
+    "json_class": "Chef::Role",
+    "run_list": [
+        "recipe[swap]"
+    ],
+    "chef_type": "role",
+    "override_attributes": {
+    }
+}
+</pre>
+
+## Reference
+
+http://www.opinionatedprogrammer.com/2011/07/creating-a-swap-file-with-chef/
---
+++ recipes/default.rb
@@ -0,0 +1,21 @@
+script 'create swapfile' do
+  interpreter 'bash'
+  not_if { File.exists?('/var/swapfile') }
+  code <<-EOF
+    mem_size=$(free -b | grep "Mem:" | awk '{print $2}')
+    dd if=/dev/zero of=/var/swapfile bs=1M count=$((${mem_size}*2/1024/1024)) &&
+    chmod 600 /var/swapfile &&
+    mkswap /var/swapfile
+  EOF
+end
+
+mount '/dev/null' do  # swap file entry for fstab
+  action :enable  # cannot mount; only add to fstab
+  device '/var/swapfile'
+  fstype 'swap'
+end
+
+script 'activate swap' do
+  interpreter 'bash'
+  code 'swapon -a'
+end
